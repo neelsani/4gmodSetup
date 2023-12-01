f86f151cf6b010c73bc154d108dc580ebe0a0084
neelsani <neelsani923@gmail.com>
2023-12-01 15:44:18 -0500

    inti

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt install python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame
+sudo python3 -m pip install MAVProxy
+
+
+sudo apt install libqmi-utils udhcpc
+
+curl -s https://install.zerotier.com | sudo bash
---
+++ start1.sh
@@ -0,0 +1,34 @@
+
+
+cd 
+sleep 5
+usbreset 1e0e:9001
+#start 4g
+while ! qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='online'; do echo trying again 1; done
+outCmd=$(while ! qmicli -d /dev/cdc-wdm0 -w; do echo trying again2; done)
+ip link set $outCmd down
+echo 'Y' | tee /sys/class/net/$outCmd/qmi/raw_ip
+ip link set $outCmd up
+while ! qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='zmp.com.attz',ip-type=4" --client-no-release-cid; do echo trying 3; done
+#qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='data641003',ip-type=4" --client-no-release-cid
+sleep 5
+
+udhcpc -i wwan0
+echo connection madesupposedly
+
+#openvpn --config /home/ivue/oracle.ovpn &
+#screen -dmS OpenVPN openvpn --config /home/ivue/oracle.ovpn
+#screen -dmS OpenVPN openvpn --config /home/ivue/ivue.ovpn
+
+
+##mavproxy stuff
+#cd /home/ivue/
+#sleep 5
+#usbreset 3162:0053
+#sleep 5
+
+#screen -dmS MavproxyIvue ./mavIvue.sh
+export LOCALAPPDATA="LOCALAPPDATA"
+echo firster
+screen -dmS MavProxyIvue bash -c "mavproxy.py --out 10.8.0.2:6767 --out 10.147.19.214:6767 --out 10.147.19.242:6767 --out 10.147.19.17:6767"  
+echo laster
